b9fc96d19f165d2802cf5674e6bd8f08ca9dd67f
Steve <cordovas@bc.edu>
2018-09-19 14:40:50 -0400

    changed the forward slash in the image and css folder

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Hello World</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="/css/main.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
     <script src="main.js"></script>
 </head>
 <body>
     <h1>Hello World</h1>
-    <img src="/images/0.jpeg" alt="">
+    <img src="images/0.jpeg" alt="">
     
 </body>
 </html>
